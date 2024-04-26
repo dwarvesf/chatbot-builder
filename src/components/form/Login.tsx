1e6ccd7e80b924ea721ca068334b9b25b4cc2e15
Ngo Lap Nguyen <ngolap.nguyen@gmail.com>
2024-04-26 14:36:50 +0700

    feat: minor update to login form

--- src/components/form/Login.tsx
+++ src/components/form/Login.tsx
@@ -1,12 +1,20 @@
+import { Button } from "@mochi-ui/core";
+import { GoogleColored } from "@mochi-ui/icons";
 import { signIn } from "next-auth/react";
-
 
 const LoginForm = () => {
   return (
-    <div className="radius flex flex-col items-center gap-2 border p-4">
-      <div>
-        <button onClick={() => signIn('google')}>sign in with gooogle</button>
-      </div>
+    <div className="mx-auto flex w-[300px] flex-col">
+      <Button
+        variant="outline"
+        color="neutral"
+        onClick={() => signIn("google")}
+      >
+        <div className="flex w-full items-center justify-between gap-4">
+          Sign in with Google
+          <GoogleColored width={24} height={24} />
+        </div>
+      </Button>
     </div>
   );
 };
