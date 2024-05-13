dc2aff9acc560bf8ea3307c7e69942430d91a9bd
Thanh Pham <zlatan.pham@gmail.com>
2024-05-13 23:09:29 +0700

    feat: add examples for validation APIs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,12 +7,14 @@ import {
   SectionHeader,
   SectionHeaderActions,
   SectionHeaderTitle,
+  Skeleton,
   Typography,
 } from '@mochi-ui/core'
 import type { GetServerSideProps, NextPage } from 'next'
 import Link from 'next/link'
 import { SeoHead } from '~/components/SeoHead'
 import { CreateBotModal } from '~/components/bot/CreateBotModal'
+import { Repeat } from '~/components/common/Repeat'
 import { ROUTES } from '~/constants/routes'
 import { getServerAuthSession } from '~/server/auth'
 import { api } from '~/utils/api'
@@ -21,6 +23,7 @@ const Index: NextPage = () => {
   const { onOpenChange, isOpen, onOpen } = useDisclosure()
   const botsQuery = api.bot.getList.useQuery()
   const bots = botsQuery?.data ?? []
+  const isFetchingBots = botsQuery.isLoading
   const isEmptyBots = bots.length === 0
 
   return (
@@ -36,34 +39,58 @@ const Index: NextPage = () => {
               color={isEmptyBots ? 'neutral' : undefined}
               variant={isEmptyBots ? 'outline' : undefined}
               onClick={onOpen}
+              disabled={isFetchingBots}
             >
               Create new Bot
             </Button>
-            <CreateBotModal isOpen={isOpen} onOpenChange={onOpenChange} />
+            <CreateBotModal
+              isOpen={isOpen}
+              onOpenChange={onOpenChange}
+              onSuccess={async () => {
+                await botsQuery.refetch()
+              }}
+            />
           </SectionHeaderActions>
         </SectionHeader>
-        {isEmptyBots ? (
-          <Card className="text-center space-y-3">
-            <Typography level="h5">No bots</Typography>
-            <Typography color="textSecondary">
-              Looks like you are missing a bot. Lets get you set up with one!
-            </Typography>
-            <Button onClick={onOpen}>Create new Bot</Button>
-          </Card>
-        ) : (
+        {isFetchingBots ? (
           <div className="grid gap-4 grid-cols-4">
-            {bots.map((bot) => (
-              <Card asChild key={bot.id}>
-                <Link href={ROUTES.BOT_DETAIL(bot?.id)}>
-                  <Avatar className="w-12 h-12" src="" />
-                  <Typography level="h6">{bot.name}</Typography>
-                  <Typography color="textSecondary">
-                    {bot.description}
-                  </Typography>
-                </Link>
+            <Repeat>
+              <Card className="space-y-5">
+                <Skeleton className="w-10 h-10 rounded-full" />
+                <div className="space-y-3">
+                  <Skeleton className="w-full h-6 rounded" />
+                  <Skeleton className="w-1/2 h-6 rounded" />
+                </div>
               </Card>
-            ))}
+            </Repeat>
           </div>
+        ) : (
+          <>
+            {isEmptyBots ? (
+              <Card className="text-center space-y-3">
+                <Typography level="h5">No bots</Typography>
+                <Typography color="textSecondary">
+                  Looks like you are missing a bot. Lets get you set up with
+                  one!
+                </Typography>
+                <Button onClick={onOpen}>Create new Bot</Button>
+              </Card>
+            ) : (
+              <div className="grid gap-4 grid-cols-4">
+                {bots.map((bot) => (
+                  <Card asChild key={bot.id}>
+                    <Link href={ROUTES.BOT_DETAIL(bot?.id)}>
+                      <Avatar className="w-12 h-12" src="" />
+                      <Typography level="h6">{bot.name}</Typography>
+                      <Typography color="textSecondary">
+                        {bot.description}
+                      </Typography>
+                    </Link>
+                  </Card>
+                ))}
+              </div>
+            )}
+          </>
         )}
       </PageContent>
     </>
