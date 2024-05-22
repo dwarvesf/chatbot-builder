35633154c4a80d94eb28d3161c3d892fef111b50
Thanh Pham <zlatan.pham@gmail.com>
2024-05-22 11:45:34 +0700

    chore: clean up files

--- src/components/BotSource/FileDocSource.tsx
+++ src/components/BotSource/FileDocSource.tsx
@@ -3,7 +3,7 @@ import { Card, Typography, toast } from '@mochi-ui/core'
 import { useParams } from 'next/navigation'
 import { BotSourceTypeEnum } from '~/model/bot-source-type'
 import { api } from '~/utils/api'
-import UploadFileModal from '../FileInput/UploadFileModal'
+import SourceFileUploader from './SourceFileUploader'
 
 export function FileDocSource() {
   const { id } = useParams()
@@ -11,9 +11,7 @@ export function FileDocSource() {
 
   const { mutate: createSource } = api.botSource.create.useMutation()
 
-  const { isPending, isSuccess, isError, error } =
-    api.botSource.createBulk.useMutation()
-
+  const { isSuccess, isError, error } = api.botSource.createBulk.useMutation()
   const { refetch: refreshSourceTable } = api.botSource.getByBotId.useQuery({
     botId,
   })
@@ -21,14 +19,14 @@ export function FileDocSource() {
   useAsyncEffect(async () => {
     if (isSuccess) {
       toast({
-        description: 'Created source from links successfully',
+        description: 'Created source from docs successfully',
         scheme: 'success',
       })
       await refreshSourceTable()
     }
     if (isError) {
       toast({
-        description: 'Failed to import links',
+        description: 'Failed to import docs',
         scheme: 'danger',
       })
       console.error(error)
@@ -39,7 +37,6 @@ export function FileDocSource() {
     if (!url) {
       return
     }
-
     createSource({ botId, url, name, typeId: BotSourceTypeEnum.File })
   }
 
@@ -48,7 +45,7 @@ export function FileDocSource() {
       <div className="flex justify-between">
         <Typography level="p4">Upload file</Typography>
       </div>
-      <UploadFileModal onSuccess={onUploadSuccess} />
+      <SourceFileUploader onSuccess={onUploadSuccess} />
     </Card>
   )
 }
