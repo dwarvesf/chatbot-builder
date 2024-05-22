2f9283f397b4a4864dc44c40a969a6dd6a6c5d0e
Thanh Pham <zlatan.pham@gmail.com>
2024-05-22 13:44:52 +0700

    fix: submit sitemap url (#76)

--- src/components/BotSource/SitemapSource/SitemapSourceLink.tsx
+++ src/components/BotSource/SitemapSource/SitemapSourceLink.tsx
@@ -1,45 +1,36 @@
-import { useAsyncEffect, useDisclosure } from '@dwarvesf/react-hooks'
+import { useAsyncEffect } from '@dwarvesf/react-hooks'
 import { zodResolver } from '@hookform/resolvers/zod'
 import {
   Button,
   Card,
   FormControl,
   FormErrorMessage,
-  IconButton,
   TextFieldInput,
   TextFieldRoot,
   Typography,
   toast,
 } from '@mochi-ui/core'
-import { TrashBinLine } from '@mochi-ui/icons'
 import { useParams } from 'next/navigation'
-import { Controller, useFieldArray, useForm } from 'react-hook-form'
+import { Controller, useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { BotSourceTypeEnum } from '~/model/bot-source-type'
 import { api } from '~/utils/api'
 
 const urlSchema = z
   .string()
   .min(1, { message: 'Required' })
-  .url({ message: 'Invalid URL' })
+  .url({ message: 'Invalid sitemap link' })
+  .regex(/\.(xml|xml\.gz)$/, { message: 'Invalid sitemap link' })
 
 const schema = z.object({
   url: urlSchema,
 })
 
-const linksSchema = z.object({
-  links: z.array(urlSchema),
-})
-
-interface LinksFormValues {
-  links: string[]
-}
-
 export function SitemapSourceLink() {
   const { id } = useParams()
   const botId = id as string
 
-  const { isOpen, onOpenChange, onOpen } = useDisclosure()
-  const { handleSubmit, control, reset } = useForm<{
+  const { handleSubmit, control, reset, formState } = useForm<{
     url: string
   }>({
     defaultValues: { url: '' },
@@ -47,21 +38,7 @@ export function SitemapSourceLink() {
     mode: 'onChange',
   })
 
-  const {
-    control: linksControl,
-    getValues,
-    formState: { errors, isDirty },
-    reset: clearLinks,
-  } = useForm<LinksFormValues>({
-    defaultValues: { links: [] },
-    resolver: zodResolver(linksSchema),
-  })
-
-  const { fields, remove, append } = useFieldArray({
-    control: linksControl,
-    // @ts-expect-error - TS doesn't like the name property
-    name: 'links',
-  })
+  const { isDirty } = formState
 
   const {
     mutate: createBotSource,
@@ -69,7 +46,7 @@ export function SitemapSourceLink() {
     isSuccess,
     isError,
     error,
-  } = api.botSource.createBulk.useMutation()
+  } = api.botSource.create.useMutation()
 
   const { refetch: refreshSourceTable } = api.botSource.getByBotId.useQuery({
     botId,
@@ -78,77 +55,37 @@ export function SitemapSourceLink() {
   useAsyncEffect(async () => {
     if (isSuccess) {
       toast({
-        description: 'Created source from links successfully',
+        description: 'Created source from sitemap successfully',
         scheme: 'success',
       })
       await refreshSourceTable()
-      clearLinks()
+      reset()
     }
     if (isError) {
       toast({
-        description: 'Failed to import links',
+        description: 'Failed to import sitemap',
         scheme: 'danger',
       })
       console.error(error)
     }
   }, [isSuccess, isError, error])
 
-  const submitBotSource = async () => {
-    const urls = [...new Set(getValues().links)]
-    if (urls.length > 0) {
-      createBotSource({ botId, urls })
-    }
-  }
-
   return (
     <Card className="mx-auto space-y-4 shadow-input">
       <div className="flex justify-between">
-        <Typography level="p4">Enter website URL</Typography>
+        <Typography level="p4">Enter sitemap URL</Typography>
       </div>
       <div>
-        <form className="space-y-2 mb-5">
-          {fields.map((field, index) => (
-            <Controller
-              key={field.id}
-              name={`links.${index}`}
-              control={linksControl}
-              render={({ field }) => (
-                <div className="flex space-x-2">
-                  <div className="flex-1">
-                    <TextFieldInput
-                      className="text-text-primary"
-                      disabled
-                      value={field.value}
-                    />
-                  </div>
-                  <IconButton
-                    onClick={(e) => {
-                      e.preventDefault()
-                      remove(index)
-                    }}
-                    size="lg"
-                    label="remove"
-                    variant="ghost"
-                    color="danger"
-                  >
-                    <TrashBinLine />
-                  </IconButton>
-                </div>
-              )}
-            />
-          ))}
-        </form>
         <form
           onSubmit={handleSubmit(({ url }) => {
             const submitUrl = url.trim()
-            if (getValues().links.includes(submitUrl)) {
-              toast({ scheme: 'danger', description: 'URL already exists' })
-              return
-            }
-            append(submitUrl)
-            reset()
+            createBotSource({
+              botId,
+              url: submitUrl,
+              typeId: BotSourceTypeEnum.Sitemap,
+            })
           })}
-          className="flex items-start space-x-2"
+          className="space-y-3"
         >
           <Controller
             name="url"
@@ -158,30 +95,20 @@ export function SitemapSourceLink() {
                 <TextFieldRoot>
                   <TextFieldInput
                     {...field}
-                    placeholder="Enter a website URL or sitemap link"
+                    placeholder="https://example.com/sitemap.xml"
                   />
                 </TextFieldRoot>
                 <FormErrorMessage>{fieldState.error?.message}</FormErrorMessage>
               </FormControl>
             )}
           />
-          <Button type="submit" variant="soft" color="neutral" className="w-20">
-            Add
-          </Button>
+          <div className="flex justify-center">
+            <Button loading={isPending} disabled={!isDirty} className="w-40">
+              Upload and Train
+            </Button>
+          </div>
         </form>
       </div>
-      {getValues().links.length > 0 ? (
-        <div className="flex justify-center">
-          <Button
-            onClick={submitBotSource}
-            disabled={!errors || !isDirty}
-            loading={isPending}
-            className="w-40"
-          >
-            Upload and Train
-          </Button>
-        </div>
-      ) : null}
     </Card>
   )
 }
