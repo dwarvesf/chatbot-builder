62de5aef78dc511fcc95a0a67b0c714bcfd83704
Tay Nguyen <nguyenvanletay@gmail.com>
2024-05-27 11:57:31 +0700

    feat: get bot source vector contents

--- src/server/api/routers/bot-source-extracted-data.ts
+++ src/server/api/routers/bot-source-extracted-data.ts
@@ -1,11 +1,88 @@
-import { and, eq, sql } from 'drizzle-orm'
+import { and, eq, inArray, sql } from 'drizzle-orm'
 import { z } from 'zod'
 import { db } from '~/server/db'
 import * as schema from '~/server/db/migration/schema'
 import { createTRPCRouter, protectedProcedure } from '../trpc'
 
 export const botSourceExtractedDataRouter = createTRPCRouter({
-  getList: protectedProcedure
+  getList: getListHandler(),
+
+  getVectorContents: getVectorsHandler(),
+})
+
+function getVectorsHandler() {
+  return protectedProcedure
+    .input(
+      z.object({
+        botSourceId: z.string().uuid(),
+        loadVectorArray: z.boolean().default(false),
+        limit: z.number().max(100).default(10),
+        offset: z.number().default(0),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const bs = await db.query.botSources.findFirst({
+        where: and(
+          eq(schema.botSources.id, input.botSourceId),
+          eq(schema.botSources.createdBy, ctx.session.user.id),
+        ),
+      })
+      if (!bs) {
+        throw new Error('Bot source not found')
+      }
+
+      // Get bsDataIds
+      const bsDataIdsRows = await db
+        .select({
+          id: schema.botSourceExtractedData.id,
+        })
+        .from(schema.botSourceExtractedData)
+        .where(eq(schema.botSourceExtractedData.botSourceId, input.botSourceId))
+      const bsDataIds = bsDataIdsRows.map((r) => r.id)
+
+      if (bsDataIds.length === 0) {
+        return {
+          data: [],
+          pagination: { total: 0, limit: input.limit, offset: input.offset },
+        }
+      }
+
+      const query = db
+        .select({
+          id: schema.botSourceExtractedDataVector.id,
+          content: schema.botSourceExtractedDataVector.content,
+        })
+        .from(schema.botSourceExtractedDataVector)
+        .limit(input.limit)
+        .offset(input.offset)
+        .$dynamic()
+
+      const whereQms = [
+        inArray(
+          schema.botSourceExtractedDataVector.botSourceExtractedDataId,
+          bsDataIds,
+        ),
+      ]
+
+      const arr = await query.where(and(...whereQms))
+
+      const countRows = await db
+        .select({
+          count: sql`COUNT(*)`,
+        })
+        .from(schema.botSourceExtractedDataVector)
+        .where(and(...whereQms))
+      const count = Number(countRows[0]?.count) ?? 0
+
+      return {
+        data: arr,
+        pagination: { total: count, limit: input.limit, offset: input.offset },
+      }
+    })
+}
+
+function getListHandler() {
+  return protectedProcedure
     .input(
       z.object({
         botSourceId: z.string().uuid(),
@@ -47,11 +124,7 @@ export const botSourceExtractedDataRouter = createTRPCRouter({
 
       return {
         data: arr,
-        pagination: {
-          total: count,
-          limit: input.limit,
-          offset: input.offset,
-        },
+        pagination: { total: count, limit: input.limit, offset: input.offset },
       }
-    }),
-})
+    })
+}
