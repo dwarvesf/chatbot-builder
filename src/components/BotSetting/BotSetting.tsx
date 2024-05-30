36205887b3e1d25fc9fe98aaa92f680b516caa79
Thanh Pham <zlatan.pham@gmail.com>
2024-05-30 10:05:19 +0700

    refactor: new form pattern

--- src/components/BotSetting/BotSetting.tsx
+++ src/components/BotSetting/BotSetting.tsx
@@ -1,15 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useAsyncEffect } from '@dwarvesf/react-hooks'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Skeleton, Typography, useToast } from '@mochi-ui/core'
+import { Typography, useToast } from '@mochi-ui/core'
 import { useParams } from 'next/navigation'
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useCallback, useMemo } from 'react'
 import { FormProvider, useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { BotModelEnum } from '~/model/bot-model'
 import { UsageLimitTypeEnum } from '~/model/usage-limit-type'
 import { api } from '~/utils/api'
 import { SaveBar } from '../SaveBar'
+import { FormSkeleton } from '../common/FormSkeleton'
 import { BotDescription } from './BotDescription'
 import { BotLimit } from './BotLimit'
 import { BotMessages } from './BotMessages'
@@ -39,10 +40,13 @@ const schema = z.object({
   usageLimitPerUserType: z.nativeEnum(UsageLimitTypeEnum),
 })
 
-export const BotSetting = () => {
+interface BotSettingFormProps {
+  defaultValues: BotSettingData
+  onSuccess?: () => Promise<any>
+}
+
+const BotSettingForm = ({ defaultValues, onSuccess }: BotSettingFormProps) => {
   const id = useParams()?.id
-  const isInitialData = useRef(false)
-  const [isFetchingData, setIsFetchingData] = useState(false)
   const { toast } = useToast()
 
   const {
@@ -53,12 +57,10 @@ export const BotSetting = () => {
     isPending,
   } = api.bot.updateBotSettings.useMutation()
 
-  const { data: sources, refetch: refetchBotSettings } =
-    api.bot.getById.useQuery(id as string)
-
   const form = useForm<BotSettingData>({
     resolver: zodResolver(schema),
     mode: 'all',
+    defaultValues,
   })
 
   const {
@@ -78,37 +80,13 @@ export const BotSetting = () => {
     [reset],
   )
 
-  useEffect(() => {
-    if (sources && !isInitialData.current) {
-      isInitialData.current = true
-      reset({
-        botId: id as string,
-        name: sources.name ?? 'Dwarves Bot',
-        description: sources.description ?? '',
-        noSourceWarningMsg:
-          sources.noSourceWarningMsg ??
-          'The bot still needs to be trained, so please add the data and train it.',
-        serverErrorMsg:
-          sources.serverErrorMsg ??
-          'Apologies, there seems to be a server error.',
-        userLimitWarningMsg:
-          sources.userLimitWarningMsg ?? "You've reached the message limit.",
-        modelId: sources.modelId ?? BotModelEnum.GPT3,
-        usageLimitPerUser: sources.usageLimitPerUser ?? 50,
-        usageLimitPerUserType:
-          sources.usageLimitPerUserType ?? UsageLimitTypeEnum.PerDay,
-      })
-      setIsFetchingData(true)
-    }
-  }, [sources])
-
   useAsyncEffect(async () => {
     if (isSuccess) {
       toast({
         description: 'Update settings successfully',
         scheme: 'success',
       })
-      await refetchBotSettings()
+      await onSuccess?.()
     }
     if (isError) {
       toast({
@@ -137,91 +115,81 @@ export const BotSetting = () => {
   }
 
   return (
-    <div>
-      {isFetchingData ? (
-        <FormProvider {...form}>
-          <form onSubmit={handleSubmit(onSubmit)} />
-          <div className="space-y-4 sm:max-w-[600px] min-h-screen">
-            <div className="space-y-4 sm:max-w-[300px]">
-              <Typography level="h6" fontWeight="lg">
-                General
-              </Typography>
-              <BotDescription />
+    <FormProvider {...form}>
+      <form onSubmit={handleSubmit(onSubmit)} />
+      <div className="space-y-4 sm:max-w-[600px] min-h-screen">
+        <div className="space-y-4 sm:max-w-[300px]">
+          <Typography level="h6" fontWeight="lg">
+            General
+          </Typography>
+          <BotDescription />
 
-              <Typography level="h6" fontWeight="lg">
-                Model
-              </Typography>
-              <BotModel />
-            </div>
-
-            <Typography level="h6" fontWeight="lg">
-              Messages
-            </Typography>
-            <BotMessages />
-
-            <Typography level="h6" fontWeight="lg">
-              Usage Limit
-            </Typography>
-            <BotLimit />
-          </div>
-          <SaveBar
-            open={isDirty || isPending || isError}
-            isLoading={isSubmitting || isPending}
-            onConfirm={handleSubmit(onSubmit)}
-            onCancel={() => reset()}
-          />
-        </FormProvider>
-      ) : (
-        <div className="min-h-screen max-w-[400px] space-y-8 animate-pulse">
-          <div className="flex flex-col w-full relative items-stretch overflow-hidden rounded gap-12">
-            <div className="flex flex-col w-full gap-4">
-              <Skeleton className="w-1/5 h-6 rounded-lg" />
-            </div>
-            <div className="flex flex-col w-full gap-4">
-              <Skeleton className="w-2/5 h-4 rounded-lg" />
-              <Skeleton className="w-full h-6 rounded-lg" />
-            </div>
-
-            <div className="flex flex-col w-full gap-4">
-              <Skeleton className="w-2/5 h-4 rounded-lg" />
-              <Skeleton className="w-full h-6 rounded-lg" />
-            </div>
-          </div>
-          <div className="flex flex-col w-full gap-4">
-            <Skeleton className="w-1/5 h-4 rounded-lg" />
-          </div>
-          <div className="flex flex-col w-full gap-4">
-            <Skeleton className="w-2/5 h-6 rounded-lg" />
-          </div>
-          <div className="flex flex-col w-full gap-4">
-            <Skeleton className="w-1/5 h-4 rounded-lg" />
-          </div>
-          <div className="flex flex-col w-full gap-4">
-            <Skeleton className="w-3/5 h-4 rounded-lg" />
-          </div>
-          <div className="flex flex-col w-full gap-4">
-            <Skeleton className="w-full h-6 rounded-lg" />
-          </div>
-          <div className="flex flex-col w-full gap-4">
-            <Skeleton className="w-3/5 h-4 rounded-lg" />
-          </div>
-          <div className="flex flex-col w-full gap-4">
-            <Skeleton className="w-full h-6 rounded-lg" />
-          </div>
-          <div className="flex flex-col w-full gap-4">
-            <Skeleton className="w-3/5 h-4 rounded-lg" />
-          </div>
-          <div className="flex flex-col w-full gap-4">
-            <Skeleton className="w-full h-6 rounded-lg" />
-          </div>
-          <div className="flex flex-col w-full gap-4">
-            <Skeleton className="w-3/5 h-4 rounded-lg" />
-          </div>
-          <div className="flex flex-col w-full gap-4">
-            <Skeleton className="w-full h-6 rounded-lg" />
-          </div>
+          <Typography level="h6" fontWeight="lg">
+            Model
+          </Typography>
+          <BotModel />
         </div>
-      )}
-    </div>
+
+        <Typography level="h6" fontWeight="lg">
+          Messages
+        </Typography>
+        <BotMessages />
+
+        <Typography level="h6" fontWeight="lg">
+          Usage Limit
+        </Typography>
+        <BotLimit />
+      </div>
+      <SaveBar
+        open={isDirty || isPending || isError}
+        isLoading={isSubmitting || isPending}
+        onConfirm={handleSubmit(onSubmit)}
+        onCancel={() => reset()}
+      />
+    </FormProvider>
+  )
+}
+
+export const BotSetting = () => {
+  const id = useParams()?.id
+  const {
+    data: sources,
+    refetch: refetchBotSettings,
+    isPending,
+  } = api.bot.getById.useQuery(id as string)
+
+  const formDefaultValues = useMemo<BotSettingData | null>(() => {
+    if (!sources) {
+      return null
+    }
+
+    return {
+      botId: id as string,
+      name: sources.name ?? 'Dwarves Bot',
+      description: sources.description ?? '',
+      noSourceWarningMsg:
+        sources.noSourceWarningMsg ??
+        'The bot still needs to be trained, so please add the data and train it.',
+      serverErrorMsg:
+        sources.serverErrorMsg ??
+        'Apologies, there seems to be a server error.',
+      userLimitWarningMsg:
+        sources.userLimitWarningMsg ?? "You've reached the message limit.",
+      modelId: sources.modelId ?? BotModelEnum.GPT3,
+      usageLimitPerUser: sources.usageLimitPerUser ?? 50,
+      usageLimitPerUserType:
+        sources.usageLimitPerUserType ?? UsageLimitTypeEnum.PerDay,
+    }
+  }, [id, sources])
+
+  if (isPending || formDefaultValues === null) {
+    return <FormSkeleton />
+  }
+
+  return (
+    <BotSettingForm
+      defaultValues={formDefaultValues}
+      onSuccess={refetchBotSettings}
+    />
   )
 }
