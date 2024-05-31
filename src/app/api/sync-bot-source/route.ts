29edb0c8ea7e3266a8e0f66fc95fd16a7d2f17b2
datnguyennnx <official.nguyendat@gmail.com>
2024-05-31 15:12:01 +0700

    feat: handle process docs, pdf and text file

--- src/app/api/sync-bot-source/route.ts
+++ src/app/api/sync-bot-source/route.ts
@@ -1,8 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unsafe-enum-comparison */
+import { DocxLoader } from '@langchain/community/document_loaders/fs/docx'
+import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf'
 import * as asyncLib from 'async'
 import { eq, type InferSelectModel } from 'drizzle-orm'
 import { type PostgresJsDatabase } from 'drizzle-orm/postgres-js'
 import { XMLParser } from 'fast-xml-parser'
+import { TextLoader } from 'langchain/document_loaders/fs/text'
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
 import { uniq } from 'lodash'
 import { uuidv7 } from 'uuidv7'
 import { z } from 'zod'
@@ -71,6 +75,8 @@ async function syncBotSource(bsId: string) {
       return syncBotSourceURL(bs)
     case BotSourceTypeEnum.SitemapFile:
       return syncBotSourceSitemapFile(bs)
+    case BotSourceTypeEnum.File:
+      return syncBotSourceFile(bs)
   }
 }
 
@@ -343,10 +349,114 @@ async function saveEmbeddings(
   content: string,
   embeddings: number[],
 ) {
+  if (!content) {
+    return
+  }
+
   await client.insert(schema.botSourceExtractedDataVector).values({
     id: uuidv7(),
     botSourceExtractedDataId: bsDataId,
     content,
     vector: embeddings,
   })
+}
+
+async function syncBotSourceFile(
+  bs: InferSelectModel<typeof schema.botSources>,
+) {
+  if (bs.statusId !== BotSourceStatusEnum.Created) {
+    throw new Error('Invalid bot source status')
+  }
+
+  if (!bs.url) {
+    throw new Error('URL is required')
+  }
+
+  const bsId = bs.id
+
+  try {
+    await db
+      .update(schema.botSources)
+      .set({ statusId: BotSourceStatusEnum.Crawling })
+      .where(eq(schema.botSources.id, bs.id))
+
+    const fileURL = bs.url
+
+    // Fetch file content
+    const res = await fetch(fileURL)
+    const sourcesText = await res.blob()
+
+    const loader = await formatFileLoader(sourcesText)
+
+    if (!loader) {
+      return
+    }
+
+    const docs = await loader.load()
+    const splitter = new RecursiveCharacterTextSplitter({
+      separators: [
+        '\n\n',
+        '\n',
+        ' ',
+        '.',
+        ',',
+        '\u200b', // Zero-width space
+        '\uff0c', // Fullwidth comma
+        '\u3001', // Ideographic comma
+        '\uff0e', // Fullwidth full stop
+        '\u3002', // Ideographic full stop
+        '',
+      ],
+    })
+
+    const contents = await splitter.splitDocuments(docs)
+    const formatContents = contents.map((doc) => doc.pageContent)
+
+    const bsDataId = uuidv7()
+    await db.insert(schema.botSourceExtractedData).values({
+      id: bsDataId,
+      botSourceId: bsId,
+      data: JSON.stringify(formatContents),
+    })
+
+    // Update status to embedding
+    console.log('Embedding bot source URL... ', bs.url)
+    await db
+      .update(schema.botSources)
+      .set({ statusId: BotSourceStatusEnum.Embedding })
+      .where(eq(schema.botSources.id, bsId))
+
+    // Embed the bot source
+    for (const { content, embeddings } of await getEmbeddingsFromContents(
+      formatContents,
+    )) {
+      // Store data to DB
+      await saveEmbeddings(db, bsDataId, content, embeddings)
+    }
+
+    // Update to completed
+    await db
+      .update(schema.botSources)
+      .set({ statusId: BotSourceStatusEnum.Completed })
+      .where(eq(schema.botSources.id, bs.id))
+  } catch (error) {
+    console.error('Error while crawling bot source ', error)
+    await db
+      .update(schema.botSources)
+      .set({ statusId: BotSourceStatusEnum.Failed })
+      .where(eq(schema.botSources.id, bs.id))
+  }
+}
+
+async function formatFileLoader(file: Blob) {
+  switch (file.type) {
+    case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+      return new DocxLoader(file)
+    case 'application/pdf':
+      return new PDFLoader(file)
+    case 'text/plain':
+      return new TextLoader(file)
+    default:
+      return false
+  }
 }
