877e09c17630e97c55df2bf4800b36ce8ccb3ca4
Nguyen Dat <official.nguyendat@gmail.com>
2024-07-12 09:30:00 +0700

    feat: change logo mochi (#115)

--- src/components/common/SeoHead/SeoHead.tsx
+++ src/components/common/SeoHead/SeoHead.tsx
@@ -11,8 +11,26 @@ export const SeoHead = (props: SeoHeadProps) => {
   return (
     <Head>
       <title>{title}</title>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      <link rel="manifest" href="/site.webmanifest" />
       <meta name="description" content={description} />
-      <link rel="icon" href="/favicon.ico" />
     </Head>
   )
 }
